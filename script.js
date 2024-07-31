7e5a8ac1d69b117206b373941d076da0ed9caf71
Tifro24 <ElijahDuma@hotmail.com>
2024-07-31 23:07:25 +0100

    almost fin
    Created the foundation which inc display, button functions etc

---
+++ script.js
@@ -0,0 +1,93 @@
+
+const img1 = document.getElementById('img1')
+const img1pv = document.getElementById('img1pv')
+
+function imageOne(){
+    img1.style.display = 'block'
+    img1pv.style.display = 'none'
+}
+
+function imageOnePV(){
+    img1pv.style.display = 'block'
+    img1.style.display = 'none'
+}
+
+const img3 = document.getElementById('img3')
+const img3apv = document.getElementById('img3apv')
+const img3bpv = document.getElementById('img3bpv')
+const img3cpv = document.getElementById('img3cpv')
+const img3dpv = document.getElementById('img3dpv')
+const img3epv = document.getElementById('img3epv')
+
+function image3(){
+    img3.style.display = 'block'
+    img3apv.style.display = 'none'
+    img3bpv.style.display = 'none'
+    img3cpv.style.display = 'none'
+    img3dpv.style.display = 'none'
+    img3epv.style.display = 'none'
+}
+
+function image3PV(){
+    img3apv.style.display = 'block'
+    img3bpv.style.display = 'block'
+    img3cpv.style.display = 'block'
+    img3dpv.style.display = 'block'
+    img3epv.style.display = 'block'
+    img3.style.display = 'none'
+}
+
+const img4 = document.getElementById('img4')
+const img4apv = document.getElementById('img4apv')
+const img4bpv = document.getElementById('img4bpv')
+
+function image4(){
+    img4.style.display = 'block'
+    img4apv.style.display = 'none'
+    img4bpv.style.display = 'none'
+}
+
+function image4PV(){
+    img4.style.display = 'none'
+    img4apv.style.display = 'block'
+    img4bpv.style.display = 'block'
+}
+
+const img5 = document.getElementById('img5')
+const img5pv = document.getElementById('img5pv')
+
+function image5(){
+    img5.style.display = 'block'
+    img5pv.style.display = 'none'
+}
+
+function image5PV(){
+    img5pv.style.display = 'block'
+    img5.style.display = 'none'
+}
+
+const img6 = document.getElementById('img6')
+const img6pv = document.getElementById('img6pv')
+
+function image6(){
+    img6.style.display = 'block'
+    img6pv.style.display = 'none'
+}
+
+function image6PV(){
+    img6pv.style.display = 'block'
+    img6.style.display = 'none'
+}
+
+const img7 = document.getElementById('img7')
+const img7pv = document.getElementById('img7pv')
+
+function image7(){
+    img7.style.display = 'block'
+    img7pv.style.display = 'none'
+}
+
+function image7PV(){
+    img7pv.style.display = 'block'
+    img7.style.display = 'none'
+}
